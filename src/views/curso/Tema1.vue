1b1bcf5bf71ad13ba4017e60a77087b2ac392015
maronita <maronita@users.noreply.github.com>
2021-03-26 18:56:34 -0500

    Tema 2 parcial

---
+++ src/views/curso/Tema1.vue
@@ -0,0 +1,79 @@
+<template lang="pug">
+.curso-main-container
+  BannerInterno(titulo="1. Hotelería")
+  .container.tarjeta--blanca.p-4.p-md-5.mb-5
+    
+    p La hotelería es una actividad económica que consiste en la prestación de servicios de alojamiento y en algunas ocasiones el servicio de alimentación, ligada a diferentes actividades usualmente el turismo.
+    p Los establecimientos comerciales dedicados a estas reciben distintos nombres, según su categoría o finalidad, aunque las denominaciones oficiales dependen de la reglamentación local, que los clasifica con criterios urbanísticos, sanitarios, impositivos o de seguridad.
+
+      .video.mb-5.mt-5
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/YXT2HTjiB3Q" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+    #t_1_1.pb-5.h2 1.1 Historia de la hotelería  y evolución
+
+    p La historia de la industria hotelera se remonta a la antigüedad (años 1000 a 500 a. de C.) cuando los comerciantes viajaban de un lugar a otro para intercambiar mercancías por hospedaje.  En ese tiempo surgieron posadas en los caminos para alojar a los viajeros y a sus caballos a cambio de dinero. Eran lugares donde los huéspedes se alojaban junto al ganado y se les ofrecía un servicio de comidas.
+
+    .row.justify-content-center.align-items-center.mt-5
+                .col-auto 
+                    img.img-a(src="@/assets/curso/images/pages/ilustraciones/1.png").img-fluid
+
+                .col
+                    p Después de la caída del imperio romano hasta los siglos X – XI el servicio de hospedaje pasó a manos de los monasterios, que prestaban este servicio por caridad. La hospitalidad se volvió un servicio voluntario y estaba protegido por la ley, también estaban los sitios donde se prestaba este servicio a cambio de un valor.
+
+                    p A partir del siglo XII y XIII las cruzadas dieron un fuerte impulso al comercio y la hospitalidad dio paso a locales que comenzaron a cobrar a cambio de alojamiento.
+
+                    p De los siglos XV al XVIII se desarrolla un servicio de posadas no solo destinadas al alojamiento de viajeros sino también como centros de actividades, negocios o para elegir a miembros del consejo eclesiástico o del parlamento.
+    .jumbotron.mt-5
+        .row.justify-content-center.align-items-center
+              .col-auto
+                  img.img-a.my-3(src="@/assets/curso/images/pages/ilustraciones/hotel.svg").img-fluid
+              .col
+                  h4.mb-0 La industria hotelera es hoy una de las principales y en algunos países europeos ocupa el primer puesto de la industria nacional.
+    
+    #t_1_2.pb-5.h2.mt-5  1.2 Normativa de formalización
+
+
+    p Ley 300 de 1996.
+    p Expide la ley general de turismo. Establece que los consejos distritales o municipales, determinarán las zonas de desarrollo turístico prioritario, así mismo autoriza a las corporaciones de aquellos municipios con menos de cien mil habitantes, que posean gran valor histórico, artístico y cultural, para que establezcan un peaje turístico, sin que la tarifa supere un salario mínimo diario legal por vehículo de uso público o comercial y medio salario mínimo diario legal por vehículo de uso particular.
+
+    .div-con-fondo1
+        .row.justify-content-center.align-items-center
+            .col-auto
+              
+              img(src='@/assets/curso/images/pages/ilustraciones/3.png').img-float
+              
+              p.centrado #[strong Dicta disposiciones sobre el ecoturismo, etnoturismo, agroturismo, ecoturismo y #[br] turismo metropolitano; Turismo de interés social, mercadeo, promoción del turismo y cooperación turística internacional, incentivos tributarios para el fomento de la actividad turística, Fondo de promoción turística, Corporación Nacional de Turismo, Registro #[br] Nacional de Turismo, derechos y obligaciones de los usuarios, control y sanciones. #[br] Crea la división de policía de turismo dentro de la dirección de servicios especializados #[br]de la Policía Nacional. #[br] Prestadores de servicios turísticos en particular, establecimientos hoteleros o de #[br]hospedaje, agencias de viajes y de turismo, transportadores de pasajeros,#[br] establecimientos de gastronomía, bares y negocios similares, establecimientos de arrendamiento de vehículos, empresas captadoras de ahorro para viajes, guías de turismo, sistema de tiempo compartido, operadores profesionales de congresos, ferias #[br] y convenciones, círculos metropolitanos turísticos.]
+
+    p.mt-5 Resolución 657 de 2005.
+    p El Ministerio de Comercio, Industria y Turismo reglamenta la categorización por estrellas de los establecimientos hoteleros o de hospedaje que prestan sus servicios en Colombia, disponiendo que sólo podrán ser categorizados por estrellas los establecimientos hoteleros o de hospedaje que en los términos del artículo 78 de la Ley 300 de 1996, se consideren como hoteles, según la definición de la norma técnica sectorial NTSH 006, para lo cual deben obtener el certificado de calidad turística, expedido por un organismo certificador debidamente acreditado por la Superintendencia de Industria y Comercio, que cuente con visto bueno de la Dirección de Turismo del Ministerio de Comercio, Industria y Turismo, sin cuya certificación no podrán utilizar en su publicidad la categorización por estrellas, so pena de ser sancionados.
+
+     #t_1_3.pb-5.h2.mt3 1.3 Normas técnicas sectoriales hoteleras para establecimientos de alojamiento y hospedaje.  
+
+    .row.justify-content-center.align-items-center.mt-5
+          .col-auto 
+              img.img-a(src="@/assets/curso/images/pages/ilustraciones/4.png").img-fluid
+  
+
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import BannerInterno from '../../components/plantilla/BannerInterno'
+export default {
+  name: 'Tema1',
+  components: {
+    BannerInterno,
+  },
+  mounted() {
+    this.increment()
+    setTimeout(() => {
+      this.decrease()
+    }, 3000)
+  },
+  methods: {
+    ...mapActions(['increment', 'decrease']),
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/views/curso/Tema2.vue
@@ -0,0 +1,188 @@
+<template lang="pug">
+.curso-main-container
+  BannerInterno(titulo="2. Establecimiento de alojamiento y hospedajes.")
+  .container.tarjeta--blanca.p-4.p-md-5.mb-5
+    
+    p Establecimiento de alojamiento y hospedajes.
+    p Son aquellos establecimientos cuya actividad principal es ofrecer alojamiento a personas, mediante precio, de forma habitual y profesional, con o sin otros servicios complementarios, mediante la denominación genérica de hotel, hostal, pensión o similar.
+
+    .video.mb-5.mt-5
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/YXT2HTjiB3Q" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+    #t_2_1.pb-5.h2 2.1 Definición
+
+    .row.justify-content-center.align-items-center.mt-5
+                .col
+
+                    p Se entiende por establecimiento hotelero o de hospedaje, el conjunto de bienes destinados por la persona natural o jurídica a prestar el servicio de alojamiento no permanente inferior a 30 días, con o sin alimentación y servicios básicos y/o complementarios o accesorios de alojamiento, mediante contrato de hospedaje, el cual es un contrato de arrendamiento, de carácter comercial y de adhesión, que una empresa dedicada a ésta actividad celebra con el propósito principal de prestar alojamiento a otra persona denominada huésped, mediante el pago del precio respectivo día a día, por un plazo inferior a 30 días.
+                .col-auto 
+                    img.img-a.my-3(src="@/assets/curso/images/pages/ilustraciones/hotel_2.svg").img-fluid
+    .jumbotron-1.mt-5
+        .row.justify-content-center.align-items-center
+              .col-auto
+                  img.img-a.my-3(src="@/assets/curso/images/pages/ilustraciones/insignia.png").img-fluid
+              .col
+                  h4.mb-0 La obligación de inscribirse en el Registro Nacional de Turismo cobija únicamente a los establecimientos que presten servicio de alojamiento exclusivamente a personas que tengan el carácter de turistas.
+
+    h6.mt-5 Capítulo II
+
+    p.mt-3 De los establecimientos hoteleros o de hospedaje
+
+    .row.mt-5.align-items-stretch.mb-0
+
+                .col-6.jumbotron-2.mb-0.jumbo-border.text-sup-2
+                    h6.mt-6.mb-0.text-left Artículo 78.  
+                .col-6.jumbotron-3.mb-0.jumbo-border.text-sup-2
+                    h6.mt-6.mb-0.text-left Artículo 79.                                  
+
+                .col-6.jumbotron-2.jumbo-border.jumbo-border
+
+                    p.mt-4 De los establecimientos hoteleros o de hospedaje. Se entiende por establecimiento hotelero o de hospedaje, el conjunto de bienes destinados por la persona natural o jurídica a prestar el servicio de alojamiento no permanente inferior a 30 días, con o sin alimentación y servicios básicos y/o complementarios o accesorios de alojamiento, mediante contrato de hospedaje.
+
+                .col-6.jumbotron-3.jumbo-border.jumbo-border
+
+                    p.mt-4 Del contrato de hospedaje. El contrato de hospedaje es un contrato de arrendamiento, de carácter comercial y de adhesión, que una empresa dedicada a esta actividad celebra con el propósito principal de prestar alojamiento a otra persona denominada huésped, mediante el pago del precio respectivo día a día, por un plazo inferior a 30 días.
+
+    #t_2_2.pb-5.h3 2.2 Características
+
+      p.mt-5 Resulta difícil hablar de una única clasificación de los establecimientos hoteleros puesto que  #[strong no todos los países disponen de una clasificación oficial] y si la tienen no se aplican los mismos criterios. Por ello, puede apreciarse que una misma categoría presente características diferentes en función del país en el que nos hallemos.
+
+      p.mt-5 Según la Norma técnica sectorial colombiana NTSH006 para la categorización de los hoteles los factores de evaluación  se dividen en:
+
+      p.mt-4 Factores de planta:
+      p.mt-3 Edificación:
+
+    .row.justify-content-center.mb-5
+        .col-auto.mb-2
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_1.svg", alt="", style="width:60px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_2.svg", alt="", style="width:100px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_3.svg", alt="", style="width:70px; display:block; margin:auto 0;")     
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_4.svg", alt="", style="width:80px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_5.svg", alt="", style="width:60px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_6.svg", alt="", style="width:80px; display:block; margin:auto 0;")
+      
+    p.mt-4 Factores del servicio:
+    br
+    .row.justify-content-center.mb-5
+        .col-auto.mb-2
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_7.svg", alt="", style="width:60px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_8.svg", alt="", style="width:80px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_9.svg", alt="", style="width:70px; display:block; margin:auto 0;")     
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_10.svg", alt="", style="width:80px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_11.svg", alt="", style="width:95px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_12.svg", alt="", style="width:50px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_13.svg", alt="", style="width:60px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_14.svg", alt="", style="width:65px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_15.svg", alt="", style="width:80px; display:block; margin:auto 0;")     
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_16.svg", alt="", style="width:60px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_17.svg", alt="", style="width:90px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_18.svg", alt="", style="width:70px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_19.svg", alt="", style="width:65px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_20.svg", alt="", style="width:80px; display:block; margin:auto 0;")
+        .col-auto.mb-4
+          img(src="@/assets/curso/images/pages/ilustraciones/ico_21.svg", alt="", style="width:100px; display:block; margin:auto 0;")
+
+    .jumbotron-4.mt-5
+        .row.justify-content-center.align-items-center
+              .col-auto
+                  img.img-a.my-3(src="@/assets/curso/images/pages/ilustraciones/star.png").img-fluid
+              .col
+                  h4.mb-0 Los criterios de evaluación para el otorgamiento del certificado de categorización por estrellas para los hoteles en las modalidades 1, 2, 3, 4 y 5.
+
+    p.mt-4 Los criterios de evaluación.
+
+    .jumbotron-5.mt-5
+        .row.justify-content-center.align-items-center
+              .jumbotron-6
+                      img.img-a(src='@/assets/curso/images/pages/ilustraciones/star_1.svg').img-icon
+                      p.text-center.mt-5
+                        | #[strong Hoteles de 1 estrella:]
+                      P.text-center deberán cumplir el 100% de los requisitos definidos en la Norma NTSH006, 178 puntos y los requisitos legales vigentes.
+              .jumbotron-6
+                      img.img-a(src='@/assets/curso/images/pages/ilustraciones/star_2.svg').img-icon
+                      p.text-center.mt-5
+                        | #[strong Hoteles de 2 estrellas:]
+                      p.text-center deberán cumplir el 100% de los requisitos definidos en la Norma NTSH006, 181 puntos y los requisitos legales vigentes.
+              .jumbotron-6
+                      img.img-a(src='@/assets/curso/images/pages/ilustraciones/star_3.svg').img-icon
+                      p.text-center.mt-5
+                        | #[strong Hoteles de 3 estrellas:]
+                      P.text-center deberán cumplir el 80% de los requisitos definidos en la Norma NTSH006, rango 208 - 260 puntos y los requisitos legales vigentes.
+              .jumbotron-6
+                      img.img-a(src='@/assets/curso/images/pages/ilustraciones/star_4.svg').img-icon
+                      p.text-center.mt-5
+                        | #[strong Hoteles de 4 estrellas:]
+                      P.text-center deberán cumplir mínimo con el 85% de los requisitos definidos en la Norma NTSH006, rango 283 - 333 puntos y los requisitos legales vigentes.
+              .jumbotron-6
+                      img.img-a(src='@/assets/curso/images/pages/ilustraciones/star_5.svg').img-icon
+                      p.text-center.mt-5
+                        | #[strong Hoteles de 5 estrellas:]
+                      p.text-center deberán cumplir mínimo con el 95% de los requisitos definidos en la Norma NTSH006, rango 349 - 337 puntos y los requisitos legales vigentes.   
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #t_2_3.pb-5.h2 tema 2.3
+    .pb-5(v-for="item of 15")
+
+    #t_2_4.pb-5.h2 tema 2.4
+    .pb-5(v-for="item of 15")
+
+    #t_2_5.pb-5.h2 tema 2.5
+    .pb-5(v-for="item of 15")
+
+    #t_2_6.pb-5.h2 tema 2.6
+    .pb-5(v-for="item of 15")
+
+    #t_2_7.pb-5.h2 tema 2.7
+    .pb-5(v-for="item of 15")
+
+    #t_2_8.pb-5.h2 tema 2.8
+    .pb-5(v-for="item of 15")
+
+</template>
+
+<script>
+import BannerInterno from '../../components/plantilla/BannerInterno'
+export default {
+  name: 'Tema1',
+  components: {
+    BannerInterno,
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
